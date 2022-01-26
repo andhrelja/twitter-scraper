60fa52d49727edaf50aa8df48c245c6d24942946
milky-boi <cres.milan@gmail.com>
2022-01-26 20:26:50 +0100

    scraping tweets from profiles added

--- src/modul.py
+++ src/modul.py
@@ -45,3 +45,41 @@ def get_friends_ids(api, user_id):
         
     return friends_ids
 
+
+def get_all_tweets(api, user_id):
+    """For given user id returns dataframe of up to last 3240 tweets from user profile.
+    Each row in the dataframe corresponds to one tweet. 
+
+    Args:
+        api (api.API): API object of tweepy.api module
+        user_id (str): user twitter id
+
+    Returns:
+        DataFrame: pandas dataframe of tweets
+    """
+
+    try:
+        alltweets = []  
+        new_tweets = api.user_timeline(user_id=user_id, count=200, tweet_mode="extended")
+        alltweets.extend(new_tweets)
+        oldest = alltweets[-1].id - 1
+        
+        while len(new_tweets) > 0:
+            new_tweets = api.user_timeline(user_id=user_id,count=200,max_id=oldest, tweet_mode="extended")
+            alltweets.extend(new_tweets)
+            oldest = alltweets[-1].id - 1
+            
+        df_tweets_on_profile = pd.DataFrame()
+        
+        for tweet in alltweets:
+            #json_str = json.dumps(tweet._json, ensure_ascii=False).encode('utf8')
+            df_json = pd.json_normalize(tweet._json)
+            df_tweets_on_profile = pd.concat([df_tweets_on_profile, df_json], axis=0) 
+    
+    except IndexError:
+        df_tweets_on_profile = pd.DataFrame()
+        
+    return df_tweets_on_profile
+
+
+    
