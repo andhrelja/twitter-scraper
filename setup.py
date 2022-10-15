069ab9297c64615cdcdbb7eb51952ea556fa6069
Andrea Hrelja <andhrelja@hotmail.com>
2022-10-15 17:32:18 +0000

    Update environment variables; add dynamic setup.py attributes population; add run.yml workflow

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup
+import json
+
+with open('README.md', 'r') as f:
+   long_description = f.read()
 
 with open('requirements.txt', 'r') as f:
    install_requires = f.readlines()
 
+with open('version.json', 'r') as f:
+   version_obj = json.load(f)
+
 setup(
    name='twitter-scraper',
-   version='0.0.2',
+   version=version_obj['version'],
+   long_description=long_description,
    packages=[
+      'twitter_scraper',
       'twitter_scraper.scrape',
       'twitter_scraper.clean',
       'twitter_scraper.graph',
       'twitter_scraper.text',
-      'twitter_scraper.utils',
-      'twitter_scraper'
+      'twitter_scraper.utils'
    ],
    package_dir={'twitter_scraper':'twitter_scraper'},
    install_requires=install_requires
