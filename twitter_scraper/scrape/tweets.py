b9f04b4eeeb4f1d2eadd5fac50eeb8494472c4cd
Andrea Hrelja <andhrelja@hotmail.com>
2022-11-11 03:03:14 +0000

    fix: Write max-tweet-ids.json for each tweet batch

--- twitter_scraper/scrape/tweets.py
+++ twitter_scraper/scrape/tweets.py
@@ -88,12 +88,10 @@ def __collect_users_tweets(conn_name: str, api: tweepy.API, pbar: tqdm):
             file_type='json', 
             indent=None
         )
+        if max_tweet_ids:
+            fileio.write_content(settings.MAX_TWEET_IDS, max_tweet_ids, 'json')
         l.release()
         pbar.update(1)
-    if max_tweet_ids:
-        l.acquire()
-        fileio.write_content(settings.MAX_TWEET_IDS, max_tweet_ids, 'json', overwrite=True)
-        l.release()
 
 
 def tweets(apis: List[dict]):
