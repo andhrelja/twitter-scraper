ec21a4483b949356abd3774418f05afaf10c84ae
Andrea Hrelja <andhrelja@hotmail.com>
2023-02-08 21:13:31 +0000

    fix: update notifications

--- twitter_scraper/utils/notification.py
+++ twitter_scraper/utils/notification.py
@@ -27,9 +27,25 @@ message = """twitter-scraper outputs:
     - found `{len_retweets}` edges
 """
 
+message_defaults = {
+    'debug': False,
+    'initial_baseline_len': 0,
+    'final_baseline_len': 0,
+    'cro_users_len': 0,
+    'nodes_len': 0,
+    'found_mentions': 0,
+    'len_mentions': 0,
+    'nodes_len': 0,
+    'found_retweets': 0,
+    'len_retweets': 0,
+    'nodes_len': 0,
+    'found_followers': 0,
+    'len_followers': 0,
+}
+
 
 def update_log_outputs():
-    global log_outputs
+    global log_outputs, message_defaults
     initial_baseline_user_ids = fileio.read_content(
         path=os.path.join(settings.INPUT_DIR, 'history', 'clean.users_{}_baseline-user-ids.json'.format(settings.folder_name)),
         file_type='json'
@@ -39,13 +55,14 @@ def update_log_outputs():
         'initial_baseline_len': len(initial_baseline_user_ids),
         'final_baseline_len': len(utils.get_baseline_user_ids())
     })
+    message_defaults.update(log_outputs)
     fileio.write_content(LOG_OUTPUT_PATH, log_outputs, 'json', overwrite=True)
 
 @DISCORD_CLIENT.event
 async def on_ready():
-    global log_outputs
+    global message_defaults
     await DISCORD_CLIENT.get_channel(CHANNEL_ID).send(
-        content=message.format(**log_outputs), 
+        content=message.format(**message_defaults), 
         file=discord.File(os.path.join(settings.LOGS_DIR, '{}.log'.format(settings.folder_name)))
     )
     await DISCORD_CLIENT.close()
