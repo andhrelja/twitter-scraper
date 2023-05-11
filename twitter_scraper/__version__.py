af0da0cbb00bab4fed357bd5c3d02b5f4f8b9523
Andrea Hrelja <andhrelja@hotmail.com>
2023-05-11 09:26:09 +0000

    fix(ci): bump python-version to 3.9

--- twitter_scraper/__version__.py
+++ twitter_scraper/__version__.py
@@ -1,7 +1,7 @@
-import importlib_resources
+import importlib.resources
 import json
 
-version_path = importlib_resources.files(__package__) / 'version.json'
+version_path = importlib.resources.files(__package__) / 'version.json'
 with open(version_path) as f:
     version_obj = json.load(f)
 __version__ = version_obj['version']
