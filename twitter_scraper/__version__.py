87651346b810354c7b294bf3e524bb1b60c649d6
Andrea Hrelja <andhrelja@hotmail.com>
2023-05-11 10:22:59 +0000

    fix(ci): upgrade setup.py to pyproject.toml

--- twitter_scraper/__version__.py
+++ twitter_scraper/__version__.py
@@ -5,3 +5,4 @@ version_path = importlib.resources.files(__package__) / 'version.json'
 with open(version_path) as f:
     version_obj = json.load(f)
 __version__ = version_obj['version']
+print("twitter_scraper {}".format(__version__))
