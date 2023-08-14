200df2a756178d58c25ec4708acbbc6781f1d3a0
Andrea Hrelja <andhrelja@hotmail.com>
2023-08-14 13:36:35 +0000

    fix(collect-tweets): handle empty clean dataframes

--- twitter_scraper/__version__.py
+++ twitter_scraper/__version__.py
@@ -1,7 +1,12 @@
-import importlib.resources
+import sys
 import json
 
-version_path = importlib.resources.files(__package__) / 'version.json'
+if sys.version_info > (3, 9):
+    import importlib.resources as pkg_resources
+else:
+    import importlib_resources as pkg_resources
+
+version_path = pkg_resources.files(__package__) / 'version.json'
 with open(version_path) as f:
     version_obj = json.load(f)
 __version__ = version_obj['version']
